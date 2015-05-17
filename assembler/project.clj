7d40f0e441082d0fa3e0edd92d9e3b53a4d878fe
Oliver Caldwell <olliec87@gmail.com>
2015-05-17 11:24:36 +0100

    Added blank Clojure assembler project

---
+++ assembler/project.clj
@@ -0,0 +1,6 @@
+(defproject assembler "0.1.0-SNAPSHOT"
+  :description "Assembler for Hack machine language"
+  :url "https://github.com/Wolfy87/nand2tetris"
+  :license {:name "Unlicense"
+            :url "http://unlicense.org/"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
