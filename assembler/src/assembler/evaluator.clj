13075eccff519af47fb1a4744f598b250247c093
Oliver Caldwell <olliec87@gmail.com>
2015-05-17 16:37:06 +0100

    Added base functions for the evaluator.

--- assembler/src/assembler/evaluator.clj
+++ assembler/src/assembler/evaluator.clj
@@ -4,4 +4,27 @@
 ;; Passes the vector to a function that calculates the full symbol table.
 ;; Passes the vector to a function that swaps symbols for addresses.
 ;; Passes the vector to a function that maps the instructions to their binary equivalent.
-;; The final binary is returned as a vector of binary strings.
+;; The final result is returned as a vector of binary strings.
+
+(defn eval
+  "Evaluates the given instructions by mapping their symbols and converting
+  them all to binary. A vector of the binary representations is returned."
+  [instructions]
+  (let [symbols (get-symbols instructions)]
+    (map get-binary (assign-symbols symbols instructions))))
+
+(defn get-symbols
+  "Constructs a symbol table for the given instructions."
+  [instructions]
+  [])
+
+(defn assign-symbols
+  "Assigns the given symbol table to the instructions, so symbols will be
+  replaced with they're numerical value."
+  [symbols instructions]
+  [])
+
+(defn get-binary
+  "Looks up the binary counterpart of the given instruction."
+  [instruction]
+  "")
--- assembler/test/assembler/evaluator_test.clj
+++ assembler/test/assembler/evaluator_test.clj
@@ -1,1 +1,19 @@
 (ns assembler.evaluator-test)
+
+(def instructions
+  [{:type :label
+    :symbol "LOOP"}
+   {:type :command
+    :comp :M
+    :jump :JMP}
+   {:type :address
+    :symbol "i"}
+   {:type :command
+    :dest :D
+    :comp :1}
+   {:type :address
+    :symbol "a"}])
+
+(defn check [f asm expected]
+  (let [result (f asm)]
+    (is (= result expected))))
