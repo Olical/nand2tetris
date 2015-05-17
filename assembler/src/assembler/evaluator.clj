f193469956fc5440d97595d6d2fc3269fb0dd0eb
Oliver Caldwell <olliec87@gmail.com>
2015-05-17 17:34:18 +0100

    Symbol table construction works

--- assembler/src/assembler/evaluator.clj
+++ assembler/src/assembler/evaluator.clj
@@ -6,17 +6,50 @@
 ;; Passes the vector to a function that maps the instructions to their binary equivalent.
 ;; The final result is returned as a vector of binary strings.
 
-(defn eval
-  "Evaluates the given instructions by mapping their symbols and converting
-  them all to binary. A vector of the binary representations is returned."
-  [instructions]
-  (let [symbols (get-symbols instructions)]
-    (map get-binary (assign-symbols symbols instructions))))
+(def default-symbol-table
+  {:SP     0
+   :LCL    1
+   :ARG    2
+   :THIS   3
+   :THAT   4
+   :R0     0
+   :R1     1
+   :R2     2
+   :R3     3
+   :R4     4
+   :R5     5
+   :R6     6
+   :R7     7
+   :R8     8
+   :R9     9
+   :R10    10
+   :R11    11
+   :R12    12
+   :R13    13
+   :R14    14
+   :R15    15
+   :SCREEN 16384
+   :KBD    24576})
+
+(defn is-symbol?
+  "Checks if the given symbol exists in the given symbol table or the default table."
+  [symbols symbol]
+  (or (contains? symbols symbol)
+      (contains? default-symbol-table symbol)))
 
 (defn get-symbols
   "Constructs a symbol table for the given instructions."
   [instructions]
-  [])
+  (loop [ins (filter #(contains? % :symbol) instructions)
+         symbols {}]
+    (if (empty? ins)
+      symbols
+      (let [symbol (keyword ((first ins) :symbol))
+            next-id (+ 16 (count symbols))]
+        (recur (rest ins)
+              (if (is-symbol? symbols symbol)
+                symbols
+                (assoc symbols symbol next-id)))))))
 
 (defn assign-symbols
   "Assigns the given symbol table to the instructions, so symbols will be
@@ -28,3 +61,10 @@
   "Looks up the binary counterpart of the given instruction."
   [instruction]
   "")
+
+(defn evaluate
+  "Evaluates the given instructions by mapping their symbols and converting
+  them all to binary. A vector of the binary representations is returned."
+  [instructions]
+  (let [symbols (merge default-symbol-table (get-symbols instructions))]
+    (map get-binary (assign-symbols symbols instructions))))
--- assembler/test/assembler/evaluator_test.clj
+++ assembler/test/assembler/evaluator_test.clj
@@ -1,4 +1,6 @@
-(ns assembler.evaluator-test)
+(ns assembler.evaluator-test
+  (:require [clojure.test :refer :all]
+            [assembler.evaluator :refer :all]))
 
 (def instructions
   [{:type :label
@@ -8,12 +10,34 @@
     :jump :JMP}
    {:type :address
     :symbol "i"}
+   {:type :label
+    :symbol "RECUR"}
    {:type :command
     :dest :D
     :comp :1}
    {:type :address
     :symbol "a"}])
 
-(defn check [f asm expected]
-  (let [result (f asm)]
+(defn check [f instructions expected]
+  (let [result (f instructions)]
     (is (= result expected))))
+
+(def check-symbols (partial check get-symbols))
+
+(deftest is-symbol-test
+  (testing "Empty symbols, so checks default."
+    (is (is-symbol? {} :R13)))
+  (testing "Checking for a custom symbol."
+    (is (is-symbol? {:foo 1} :foo)))
+  (testing "A symbol that doesn't exist."
+    (is (not (is-symbol? {} :foo)))))
+
+(deftest get-symbols-test
+  (testing "Empty instructions return nothing."
+    (check-symbols [] {}))
+  (testing "Instructions with unresolved symbols resolve to the default symbol table."
+    (check-symbols instructions
+                   {:LOOP 16
+                    :i 17
+                    :RECUR 18
+                    :a 19})))
