f4c52661c7f038b588edb240058533c8929d5f12
Oliver Caldwell <olliec87@gmail.com>
2015-05-18 22:42:43 +0100

    Finished core stdin/out tool

--- assembler/src/assembler/core.clj
+++ assembler/src/assembler/core.clj
@@ -1,6 +1,14 @@
-(ns assembler.core)
+(ns assembler.core
+  (:require [assembler.evaluator :refer [evaluate]]
+            [assembler.parser :refer [parse-source]]
+            [clojure.string :as str]))
 
 (defn -main
   "Takes stdin, pipes it through the assembler parts and pipes the result through stdout."
   []
-  (println (slurp *in*)))
+  (->> (slurp *in*)
+       parse-source
+       evaluate
+       (remove nil?)
+       (str/join "\n")
+       println))
